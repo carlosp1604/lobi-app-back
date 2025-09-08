0aeb5fa9546ea12d51589d4007f23120378d5fbd
Carlos Ponton <carlitosponton@hotmail.com>
2025-09-08 17:45:02 +0200

    Add an initial config for eslint/prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import unusedImports from 'eslint-plugin-unused-imports'
+import importPlugin from 'eslint-plugin-import'
 
 export default tseslint.config(
   {
@@ -11,6 +13,19 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
+  {
+    plugins: {
+      'unused-imports': unusedImports,
+    import: importPlugin
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: "./tsconfig.json",
+        },
+      }
+    }
+  },
   {
     languageOptions: {
       globals: {
@@ -26,9 +41,42 @@ export default tseslint.config(
   },
   {
     rules: {
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        }
+      ],
+      'import/no-unresolved': 'error',
+      'import/no-relative-parent-imports': 'off',
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [ '../**' ]
+        }
+      ]
     },
   },
+  {
+    files: [
+      '**/*.spec.ts',
+      '**/*.spec.tsx',
+      '**/*.e2e-spec.ts',
+      '**/__tests__/**/*.{ts,tsx}',
+      'test/**/*.{ts,tsx}',
+    ],
+    rules: {
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+    },
+  }
 );
