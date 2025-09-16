38775a1300ea782581d58acf6841f878cdd5754e
Carlos Ponton <carlitosponton@hotmail.com>
2025-09-16 21:52:19 +0200

    Remove unused utility and its test

--- src/test/utils/fast-check-helper.ts
+++
@@ -1,7 +0,0 @@
-import fc from 'fast-check'
-
-export function checkProperty(label: string, property: fc.IProperty<any>) {
-  test(label, () => {
-    fc.assert(property, { verbose: true })
-  })
-}
