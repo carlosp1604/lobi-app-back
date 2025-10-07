0a86f7e97adc40776f10a89228ac859861150c0b
Carlos Ponton <carlitosponton@hotmail.com>
2025-10-07 15:29:32 +0200

    Add tests for TypeOrmManagerResolver

--- src/modules/Shared/Infrastructure/TypeOrmManagerResolver.ts
+++ src/modules/Shared/Infrastructure/TypeOrmManagerResolver.ts
@@ -1,12 +1,9 @@
-import { Injectable } from '@nestjs/common'
-import { InjectDataSource } from '@nestjs/typeorm'
 import { DataSource, EntityManager } from 'typeorm'
 import { TxContext } from '~/src/modules/Shared/Application/TxContext'
 import { TypeOrmTxContext } from '~/src/modules/Shared/Infrastructure/TypeOrmUnitOfWork'
 
-@Injectable()
 export class TypeOrmManagerResolver {
-  constructor(@InjectDataSource() private readonly ds: DataSource) {}
+  constructor(private readonly ds: DataSource) {}
 
   resolve(context?: TxContext): EntityManager {
     return context instanceof TypeOrmTxContext ? context.manager : this.ds.manager
---
+++ src/test/modules/Shared/Infrastructure/TypeOrmManagerResolver.spec.ts
@@ -0,0 +1,36 @@
+import { DataSource, EntityManager } from 'typeorm'
+import { TypeOrmManagerResolver } from '~/src/modules/Shared/Infrastructure/TypeOrmManagerResolver'
+import { TypeOrmTxContext } from '~/src/modules/Shared/Infrastructure/TypeOrmUnitOfWork'
+
+describe('TypeOrmManagerResolver', () => {
+  const defaultManager = {} as unknown as EntityManager
+  const transactionManager = {} as unknown as EntityManager
+
+  const mockedDataSource = { manager: defaultManager } as unknown as DataSource
+
+  it('should return the transaction manager when context is a TypeOrmTxContext', () => {
+    const resolver = new TypeOrmManagerResolver(mockedDataSource)
+    const context = new TypeOrmTxContext(transactionManager)
+
+    const entityManager = resolver.resolve(context)
+
+    expect(entityManager).toBe(context.manager)
+  })
+
+  it('should return the default DataSource manager when context is undefined', () => {
+    const resolver = new TypeOrmManagerResolver(mockedDataSource)
+
+    const entityManager = resolver.resolve()
+
+    expect(entityManager).toBe(defaultManager)
+  })
+
+  it('should return the default DataSource manager when context is not a TypeOrmTxContext', () => {
+    const fakeContext: any = {}
+    const resolver = new TypeOrmManagerResolver(mockedDataSource)
+
+    const entityManager = resolver.resolve(fakeContext)
+
+    expect(entityManager).toBe(defaultManager)
+  })
+})
