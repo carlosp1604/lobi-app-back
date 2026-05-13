445a9842562a5b0042f13cc5f0231f2379cdf0c3
Carlos Ponton <carlitosponton@hotmail.com>
2026-05-13 18:47:03 +0200

    feat(participation): add canEnable/enable methods to Participation domain model

--- src/modules/Activity/Domain/Participation/ParticipationDomainException.ts
+++ src/modules/Activity/Domain/Participation/ParticipationDomainException.ts
@@ -1,9 +1,11 @@
+import { Identifier } from '~/src/modules/Shared/Domain/ValueObject/Identifier'
 import { DomainException, DomainExceptionContext } from '~/src/modules/Exception/Domain/DomainException'
 
 export class ParticipationDomainException extends DomainException {
   public readonly __brand = 'ParticipationDomainException' as const
 
-  public static inactiveParticipationId = 'participation_domain_inactive_participation'
+  public static readonly inactiveParticipationId = 'participation_domain_inactive_participation'
+  public static readonly participationIsStillActiveId = 'participation_domain_participation_is_still_active'
 
   private constructor(message: string, id: string, context: DomainExceptionContext = {}) {
     super(message, id, ParticipationDomainException.name, context)
@@ -14,5 +16,16 @@ export class ParticipationDomainException extends DomainException {
       userId,
       activityId,
     })
+  }
+
+  public static participationIsStillActive(activityId: Identifier, participantId: Identifier) {
+    return new ParticipationDomainException(
+      'Cannot enable participation. This participation is still active',
+      this.participationIsStillActiveId,
+      {
+        activityId: activityId.value,
+        participantId: participantId.value,
+      },
+    )
   }
 }
